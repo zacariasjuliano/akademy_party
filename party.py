812ef83e7c20d56bffbd0780c5b5d60e4fd75080
Zacarias Juliano <zacariasjuliano@gmail.com>
2024-07-06 15:22:56 +0100

    Update Files

--- party.py
+++ party.py
@@ -4,29 +4,29 @@ from trytond.pyson import Eval, Not, Bool
 
 
 _GENDER = [
-        (None, ''),
-        ('masculino', 'Masculino'), ('femenino', 'Femenino'),
-        ('nao_binário', 'Não-Binário'), ('genero_fluido', 'Gênero Fluído'),
-        ('agenero', 'Agênero'), ('transgenero', 'Transgênero'),
-        ('bigenero', 'Bigenero'), ('generoqueer', 'Gêneroqueer'),
-        ('two_spirit', 'Two-Spirit'), ('pangenero', 'Pangênero')
-    ]
+    (None, ''),
+    ('masculino', 'Masculino'), ('femenino', 'Femenino'),
+    ('nao_binário', 'Não-Binário'), ('genero_fluido', 'Gênero Fluído'),
+    ('agenero', 'Agênero'), ('transgenero', 'Transgênero'),
+    ('bigenero', 'Bigenero'), ('generoqueer', 'Gêneroqueer'),
+    ('two_spirit', 'Two-Spirit'), ('pangenero', 'Pangênero')
+]
 
 _MARITALSTATUS =  [
-        (None, ''),
-        ('solteiro(a)', 'Solteiro(a)'), ('casado(a)', 'Casado(a)'),
-        ('divorciado(a)', 'Divorciado(a)'), ('viuvo(a)', 'Víuvo(a)'),
-        ('separado(a)', 'Separado(a)'), ('uniao_estavel', 'União Estável'),
-        ('relacionamento_aberto', 'Relacionamento Aberto'), ('relacao_domestica_registrada', 'Relação Doméstica Registrada'),
-        ('casamento_de_fato', 'Casamento de Fato'), ('casamento_civil', 'Casamento Civil'),
-        ('casamento_religioso', 'Casamento Religioso')
-    ] 
+    (None, ''),
+    ('solteiro(a)', 'Solteiro(a)'), ('casado(a)', 'Casado(a)'),
+    ('divorciado(a)', 'Divorciado(a)'), ('viuvo(a)', 'Víuvo(a)'),
+    ('separado(a)', 'Separado(a)'), ('uniao_estavel', 'União Estável'),
+    ('relacionamento_aberto', 'Relacionamento Aberto'), ('relacao_domestica_registrada', 'Relação Doméstica Registrada'),
+    ('casamento_de_fato', 'Casamento de Fato'), ('casamento_civil', 'Casamento Civil'),
+    ('casamento_religioso', 'Casamento Religioso')
+]
 
 
 class Party(metaclass=PoolMeta):
     'Party'
     __name__= 'party.party'
-    _rec_name = 'name'
+    _rec_name = 'name'    
 
     is_person = fields.Boolean(
         "Pessoa", 
@@ -61,17 +61,17 @@ class Party(metaclass=PoolMeta):
     bi_number = fields.Char(
         'B.I nº', size=15,
         states={
-            'invisible': Not(Bool(Eval('is_person'))), 
-            'required': Bool(Eval('is_person'))
+            'invisible': Not(Bool(Eval('is_person')))
         }, help="Número do bilhete de intidade.")
     father = fields.Many2One(
         'party.party', 'Pai',
         states={
             'invisible': Not(Bool(Eval('is_person')))
-        }, help="Nome do pai.")
+        }, ondelete="RESTRICT", help="Nome do pai.")
     mother = fields.Many2One(
         'party.party', 'Mãe',
         states={
             'invisible': Not(Bool(Eval('is_person')))
-        }, help="Nome da mãe.")
-	
+        }, ondelete="RESTRICT", help="Nome da mãe.")
+
+
